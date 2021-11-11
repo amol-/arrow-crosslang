baac32f7113ec8abcc97e2c57ad791dcb8e651a0
Alessandro Molina <amol@turbogears.org>
2021-11-11 12:40:43 +0100

    Improve R example

--- R2py/addthree.py
+++ R2py/addthree.py
@@ -12,13 +12,16 @@ print("PYTHON ARRAY", array)
 
 from pyarrow.cffi import ffi as arrow_c
 
-c_array = arrow_c.new("struct ArrowArray*")
-c_array_ptr = int(arrow_c.cast("uintptr_t", c_array))
-array._export_to_c(c_array_ptr)
+with arrow_c.new("struct ArrowArray*") as c_array, \
+     arrow_c.new("struct ArrowSchema*") as c_schema:
+    c_array_ptr = int(arrow_c.cast("uintptr_t", c_array))
+    c_schema_ptr = int(arrow_c.cast("uintptr_t", c_schema))
 
-c_schema = arrow_c.new("struct ArrowSchema*")
-c_schema_ptr = int(arrow_c.cast("uintptr_t", c_schema))
-array.type._export_to_c(c_schema_ptr)
+    array._export_to_c(c_array_ptr)
+    array.type._export_to_c(c_schema_ptr)
 
-x = addthree(str(c_array_ptr), str(c_schema_ptr))
-print("AFTER R FUNCTION", x)
+    r_result = addthree(str(c_array_ptr), str(c_schema_ptr))
+    r_result["export_to_c"](float(c_array_ptr), float(c_schema_ptr))
+
+    py_result = pyarrow.Array._import_from_c(c_array_ptr, c_schema_ptr)
+    print("RESULT", py_result)
